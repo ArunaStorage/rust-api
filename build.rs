14960acc538e5ec3c37883d2614773281bcbb9b3
St4NNi <sb@pus.de>
2023-06-02 10:33:23 +0200

    feat: Pre-compiled bundler

--- build.rs
+++ build.rs
@@ -29,6 +29,18 @@ fn main() {
         protos.push(rel_path);
     }
 
+    let service_entries = fs::read_dir("./src/protos/aruna/api/bundler/services/v1/").unwrap();
+
+    for entry in service_entries {
+        let dir = entry.unwrap();
+        let rel_path = format!(
+            "{}{}",
+            "./src/protos/aruna/api/bundler/services/v1/",
+            dir.file_name().to_str().unwrap()
+        );
+        protos.push(rel_path);
+    }
+
     let service_entries = fs::read_dir("./src/protos/aruna/api/internal/v1/").unwrap();
 
     for entry in service_entries {
@@ -41,8 +53,7 @@ fn main() {
         protos.push(rel_path);
     }
 
-    tonic_build
-        ::configure()
+    tonic_build::configure()
         .build_server(true)
         .out_dir("./src/aruna")
         .compile(
@@ -51,12 +62,11 @@ fn main() {
                 "./src/protos".to_string(),
                 "./src/protos/aruna/api/google".to_string(),
                 "./src/protos/aruna/api/protoc-gen-openapiv2".to_string(),
-            ]
+            ],
         )
         .unwrap();
 
-    tonic_build
-        ::configure()
+    tonic_build::configure()
         .build_server(true)
         .build_transport(false)
         .out_dir("./src/aruna_no_transport")
@@ -66,7 +76,7 @@ fn main() {
                 "./src/protos".to_string(),
                 "./src/protos/aruna/api/google".to_string(),
                 "./src/protos/aruna/api/protoc-gen-openapiv2".to_string(),
-            ]
+            ],
         )
         .unwrap();
 }
